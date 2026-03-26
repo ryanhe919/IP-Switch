08635b7bac8bdde50d2c45b41252c209fce478e2
Yufan He <ryanhe0919@gmail.com>
2026-03-26 13:52:46 +0800

    Initial Commit

---
+++ IP-Switch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IP-Switch
+//
+//  Created by Yufan He on 2026/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IP-Switch/IP_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IP_SwitchApp.swift
+//  IP-Switch
+//
+//  Created by Yufan He on 2026/3/26.
+//
+
+import SwiftUI
+
+@main
+struct IP_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
